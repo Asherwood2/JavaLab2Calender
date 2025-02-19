e84641625c9c8076a62f4dbba177b21261b2eaa4
Austin Sherwood <asherwood349@cub.uca.edu>
2025-02-18 18:00:13 -0600

    Event tester finishing work for meeting tests

--- src/EventTester.java
+++ src/EventTester.java
@@ -16,7 +16,6 @@ public class EventTester {
         Deadline Deadline1 = new Deadline("Implementing code for time machine", datetime);
         System.out.println(Deadline1.getName());
         System.out.println(Deadline1.getDateTime()); // Use formatter here
-        System.out.println(Deadline1.getDateTime());
         System.out.println(Deadline1.isComplete());
         Deadline1.complete();
         System.out.println(Deadline1.isComplete());
@@ -34,11 +33,22 @@ public class EventTester {
         LocalDateTime Start_time = LocalDateTime.of(2025, 3, 30, 14, 0);
         LocalDateTime End_time = LocalDateTime.of(2025, 3, 30, 15, 0);
             Meeting meeting1 = new Meeting("Meeting for goals before summer", Start_time, End_time, "2nd floor conference room");
+        System.out.println(meeting1.getName());
+        System.out.println(meeting1.getDuration());
+        System.out.println(meeting1.isComplete());
+        meeting1.complete();
+        System.out.println(meeting1.isComplete());
     }
     public static void meeting2(){
         LocalDateTime Start_time = LocalDateTime.of(2025, 4, 1, 10, 0);
         LocalDateTime End_time = LocalDateTime.of(2025, 4, 1, 11, 0);
-        Meeting meeting1 = new Meeting("Follow up on code for time machine", Start_time, End_time, "Remote");
+        Meeting meeting2 = new Meeting("Follow up on code for time machine", Start_time, End_time, "Remote");
+        System.out.println(meeting2.getName());
+        System.out.println(meeting2.getDuration());
+        System.out.println(meeting2.isComplete());
+        meeting2.complete();
+        System.out.println(meeting2.isComplete());
+
     }
 }
 
